c1cfd6d0d19a7de16284d52e3bf948324786718b
Gab Caser <caserogabriela@gmail.com>
2025-05-06 17:30:34 +0200

    repositores for Admin, Franchise, Game and Player created

---
+++ src/main/java/com/project_adventure/lab/repositories/FranchiseRepository.java
@@ -0,0 +1,7 @@
+package com.project_adventure.lab.repositories;
+
+import com.project_adventure.lab.models.Franchise;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FranchiseRepository extends JpaRepository<Franchise, Long> {
+}
