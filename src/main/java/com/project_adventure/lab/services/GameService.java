0bf51599be3de90e8d01f906b581d64fc097a41b
Gab Caser <caserogabriela@gmail.com>
2025-05-07 14:57:20 +0200

    GameService Initialized

--- src/main/java/com/project_adventure/lab/services/GameService.java
+++ src/main/java/com/project_adventure/lab/services/GameService.java
@@ -1,4 +1,19 @@
 package com.project_adventure.lab.services;
 
+import com.project_adventure.lab.models.Game;
+import com.project_adventure.lab.repositories.GameRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class GameService {
+
+    @Autowired
+    private GameRepository gameRepository;
+
+    public List<Game> getAllGames() {
+        return gameRepository.findAll();
+    }
 }
