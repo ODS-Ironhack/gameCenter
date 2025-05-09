41aa7ec9347f0fe93a22580df9d160def90e8dd9
Gab Caser <caserogabriela@gmail.com>
2025-05-09 13:59:54 +0200

    JwtServicesTest initialized

--- src/test/java/com/project_adventure/lab/services/JwtServiceTest.java
+++ src/test/java/com/project_adventure/lab/services/JwtServiceTest.java
@@ -1,4 +1,18 @@
 package com.project_adventure.lab.services;
 
+import com.project_adventure.lab.repositories.UserRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
 public class JwtServiceTest {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Test
+    @DisplayName("Generate a token correctly")
+    void
 }
